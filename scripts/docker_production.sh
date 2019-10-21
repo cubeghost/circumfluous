2c04a74f077f8f1e3bca398c43fca77420af58bf
alex baldwin <ghostbaldwin@gmail.com>
2019-10-20 21:54:53 -0400

    script change

---
+++ scripts/docker_production.sh
@@ -0,0 +1,7 @@
+COMMAND=$1
+COMPOSES="-f docker-compose.yml -f docker-compose.production.yml"
+
+case $COMMAND in
+    up) docker-compose $COMPOSES up -d;;
+    restart) docker-compose $COMPOSES restart;;
+esac
