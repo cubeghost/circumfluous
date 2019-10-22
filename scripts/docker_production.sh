3165073f4709289bc5ac4a1f66e68c73447c5a95
alex baldwin <ghostbaldwin@gmail.com>
2019-10-21 23:09:16 -0400

    add shell

--- scripts/docker_production.sh
+++ scripts/docker_production.sh
@@ -4,5 +4,6 @@ COMPOSES="-f docker-compose.yml -f docker-compose.production.yml"
 case $COMMAND in
     up) docker-compose $COMPOSES up -d;;
     restart) docker-compose $COMPOSES restart;;
+    shell) docker-compose $COMPOSES exec sh;;
     *) echo "$COMMAND not implemented :(";;
 esac
