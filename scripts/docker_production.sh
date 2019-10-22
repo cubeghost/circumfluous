01db0322974512a0d80fd19db1882c20c07375e3
alex baldwin <ghostbaldwin@gmail.com>
2019-10-21 23:01:55 -0400

    swap root to tagreplacer for testing

--- scripts/docker_production.sh
+++ scripts/docker_production.sh
@@ -4,4 +4,5 @@ COMPOSES="-f docker-compose.yml -f docker-compose.production.yml"
 case $COMMAND in
     up) docker-compose $COMPOSES up -d;;
     restart) docker-compose $COMPOSES restart;;
+    *) echo "$COMMAND" not implemented :("
 esac
